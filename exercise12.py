1b7e438d99452a5c26cb843fec5a89478c73a328
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 14:59:51 -0300

    Add files via upload

---
+++ exercise12.py
@@ -0,0 +1,3 @@
+p = int(input("Digite o preço de um produto e te mostrarei como ele ficará com 5% de desconto: "))
+d = p*0.95
+print("O preço antigo era: {} reais, agora com desconto está: {} reais".format(p, d))
